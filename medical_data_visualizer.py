5c77175ea38404c043bf8e7d1d7bd41a8ae9480c
Danilo de Castro <123521960+danilocneto@users.noreply.github.com>
2025-09-07 21:49:18 -0300

    feat: add code

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,69 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+# definição de datarame
+df = pd.read_csv('medical_examination.csv')    
 
-# 2
-df['overweight'] = None
+# criação da coluna de sobrepeso 
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
+df["gluc"] = (df["gluc"] > 1).astype(int)
 
-# 3
 
-
-# 4
+# Função Catplot
 def draw_cat_plot():
-    # 5
-    df_cat = None
 
+    # pd.melt() define as colunas abaixo como variáveis de identificação e torna em formato longo o df
+    df_cat = pd.melt(
+        df,
+        id_vars=["cardio"],
+        value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"]
+    )
 
-    # 6
-    df_cat = None
-    
+    # Agrupar os dados, .size faz a contagem de quantos dados tem em cada grupo, cria uma coluna 'total' com a contagem de cada grupo
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]) \
+                   .size().reset_index(name="total")
 
-    # 7
+    # Criar gráfico 
+    fig = sns.catplot(
+        data=df_cat,
+        x="variable", y="total", hue="value",
+        col="cardio", kind="bar"
+    ).fig
 
-
-
-    # 8
-    fig = None
-
-
-    # 9
-    fig.savefig('catplot.png')
+    fig.savefig("imgs/catplot.png")
     return fig
 
 
-# 10
+# Função Heatmap
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Limpeza dos dados
+    df_heat = df[
+        (df["ap_lo"] <= df["ap_hi"]) &
+        (df["height"] >= df["height"].quantile(0.025)) &
+        (df["height"] <= df["height"].quantile(0.975)) &
+        (df["weight"] >= df["weight"].quantile(0.025)) &
+        (df["weight"] <= df["weight"].quantile(0.975))
+    ]
 
-    # 12
-    corr = None
+    # Matriz de correlação que será usada para o heatmap
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # Máscara para o triângulo superior
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    # Configuração da figura
+    fig, ax = plt.subplots(figsize=(12, 12))
 
+    # Heatmap
+    sns.heatmap(
+        corr,
+        mask=mask,
+        annot=True,
+        fmt=".1f",
+        center=0,
+        square=True,
+        cbar_kws={"shrink": 0.5}
+    )
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
-    fig.savefig('heatmap.png')
+    fig.savefig("imgs/heatmap.png")
     return fig
